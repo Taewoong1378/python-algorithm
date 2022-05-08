ae0e5d8a471cfacc47725059f157667e17e2784a
TAEWOONG KANG <ktw2378@korea.ac.kr>
2022-05-08 14:18:58 +0900

    Feat: 전체 정렬(선택 정렬, 버블 정렬, 삽입 정렬, 병합 정렬, 퀵 정렬) 코딩 완료

--- sort/selection_sort.py
+++ sort/selection_sort.py
@@ -1,12 +1,22 @@
+# 삽입 정렬
+# 입력: 리스트 a
+# 출력: 없음(입력으로 주어진 a가 정렬됨)
+
 def selection_sort(a):
-  n = len(a)
-  for i in range(0, n - 1):
-    min_idx = i
-    for j in range(i + 1, n):
-      if a[min_idx] > a[j]:
-        min_idx = j
-    a[i], a[min_idx] = a[min_idx], a[i]
+	n = len(a)
+	for i in range(0, n - 1):
+		# i번 위치부터 끝까지 자료 값 중 최솟값의 위치를 찾음
+		min_idx = i
+		for j in range(i + 1, n):
+			if a[j] < a[min_idx]:
+				min_idx = j
+		
+		# 찾은 최솟값을 i번 위치로
+		a[i], a[min_idx] = a[min_idx], a[i]
+
 
 d = [2, 4, 5, 1, 3]
 selection_sort(d)
 print(d)
+
+# 결과 : [1, 2, 3, 4, 5]
